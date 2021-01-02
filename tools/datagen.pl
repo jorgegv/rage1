b0a97c0dbf352b4c51b7c96ffda2de549463eea9
Jorge Gonzalez <jorge.gonzalez@daikon.es>
2021-01-02 14:52:26 +0100

    Fixed errata in license in all source files.

--- tools/datagen.pl
+++ tools/datagen.pl
@@ -5,7 +5,7 @@
 ## RAGE1 - Retro Adventure Game Engine, release 1
 ## (c) Copyright 2020 Jorge Gonzalez Villalonga <jorgegv@daikon.es>
 ## 
-## This code is ublished under a GNU GPL license version 3 or later.  See
+## This code is published under a GNU GPL license version 3 or later.  See
 ## LICENSE file in the distribution for details.
 ## 
 ################################################################################
