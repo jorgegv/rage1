e633c6f35a7df9285faae5604c860a49a053737b
Jorge Gonzalez <jorge.gonzalez@daikon.es>
2021-02-16 17:41:21 +0100

    fix: concise information from memmap.sh

--- tools/memmap.sh
+++ tools/memmap.sh
@@ -5,4 +5,4 @@ if [ $# != 1 ]; then
 	exit 1
 fi
 
-cut -f1 -d\; < "$1" | perl -e 'while(<>){($a,$b) = split(/\s*=\s*/); chomp $b; push @l,[$b,$a];};print join("\n", map { sprintf "%s %s",@$_ } sort { $a->[0] cmp $b->[0] } @l );print "\n"'
+cut -f1 -d\; < "$1" | perl -e 'while(<>){($a,$b) = split(/\s*=\s*/); chomp $b; push @l,[$b,$a];};print join("\n", map { sprintf "%s %s",@$_ } sort { $a->[0] cmp $b->[0] } @l );print "\n"'|grep -E '__(code|data|bss)_compiler_(head|size)'
