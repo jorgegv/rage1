cee4c6b984cc560470b49c13101629ce2ef75646
Jorge Gonzalez <jorge.gonzalez@daikon.es>
2021-03-14 19:31:26 +0100

    Added decial output to memmap.sh

--- tools/memmap.sh
+++ tools/memmap.sh
@@ -5,4 +5,8 @@ if [ $# != 1 ]; then
 	exit 1
 fi
 
-cut -f1 -d\; < "$1" | perl -e 'while(<>){($a,$b) = split(/\s*=\s*/); chomp $b; push @l,[$b,$a];};print join("\n", map { sprintf "%s %s",@$_ } sort { $a->[0] cmp $b->[0] } @l );print "\n"'|grep -E '__(code|data|bss)_compiler_(head|size)'
+cut -f1 -d\; < "$1" \
+	| perl -e 'while(<>){($a,$b) = split(/\s*=\s*/); chomp $b; push @l,[$b,$a];};print join("\n", map { sprintf "%s %s",@$_ } sort { $a->[0] cmp $b->[0] } @l );print "\n"' \
+	| grep -E '__(code|data|bss)_compiler_(head|size)' \
+	| sort -k 2 -d \
+	| perl -pe 's/^\$(\w+)\s+(.+)/\$$1 \[@{[sprintf "%5d",hex("0x".$1)]}\] $2/g'
