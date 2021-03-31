76496952a2a44239d82849a2163a10d86e10aabc
Jorge Gonzalez <jorge.gonzalez@daikon.es>
2021-03-31 12:34:41 +0200

    game_loop: fixed wrong initial seeding of PRNG #26

--- engine/src/game_loop.c
+++ engine/src/game_loop.c
@@ -125,7 +125,7 @@ void run_main_game_loop(void) {
    // and the controller has been selected. This involves the human user, and so
    // introduces a random factor in the frame and seconds counter, which are then
    // used to set the initial seed of the PRNG
-   srand( ( current_time.sec << 8 ) & current_time.frame );
+   srand( ( current_time.sec << 8 ) | current_time.frame );
 
    // reset game vars and setup initial state
    game_state_reset_initial();
