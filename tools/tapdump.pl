7780482aa7f739360855957a5cb10853591ee233
Jorge Gonzalez <jorge.gonzalez@daikon.es>
2021-08-21 14:02:53 +0200

    tools: tapdump.pl cosmetic changes in output

--- tools/tapdump.pl
+++ tools/tapdump.pl
@@ -47,29 +47,28 @@ sub output_data_block_info {
         if ( $data_type eq 'PROGRAM' ) {
             my $auto_start = $data->[ 13 ] + 256 * $data->[ 14 ];
             my $program_length = $data->[ 15 ] + 256 * $data->[ 16 ];
-            printf "  Header block: Type = %s, File Name = \"%s\", Length = %d, Autostart = %d, Program Length = %d\n",
+            printf "  [Header] Type: %s, File Name: \"%s\", Length: %d, Autostart: %d, Program Length: %d\n",
                 $data_type, $filename, $length, $auto_start, $program_length;
         }
         if ( $data_type eq 'NUMVAR' ) {
             my $var_name = @$data[ 13 .. 14 ];
-            printf "  Header block:  Type = %s, File Name = \"%s\", Length = %d, Variable Name = %s\n",
+            printf "  [Header] Type: %s, File Name: \"%s\", Length: %d, Variable Name: %s\n",
                 $data_type, $filename, $length, $var_name;
         }
         if ( $data_type eq 'STRVAR' ) {
             my $var_name = @$data[ 13 .. 14 ];
-            printf "  Header block: Type = %s, File Name = \"%s\", Length = %d, Variable Name = %s\n",
+            printf "  [Header] Type: %s, File Name: \"%s\", Length: %d, Variable Name: %s\n",
                 $data_type, $filename, $length, $var_name;
         }
         if ( $data_type eq 'CODE' ) {
             my $start_address = $data->[ 13 ] + 256 * $data->[ 14 ];
-            printf "  Header block: Type = %s, File Name = \"%s\", Length = %d, Start Address = %d (0x%04X)\n",
+            printf "  [Header] Type: %s, File Name: \"%s\", Length: %d, Start Address: %d (0x%04X)\n",
                 $data_type, $filename, $length, $start_address, $start_address;
         }
     } elsif ( $block_type eq 'DATA' ) {
-        printf "  Data block: Size = %d\n",
+        printf "  [Data] Size: %d\n",
             scalar( @$data );
     }
-    print "\n";
 }
 
 ###
@@ -91,9 +90,9 @@ while ( $pos < $file_size ) {
     my $data = [ @$bytes[ $pos .. ( $pos + $size - 2 - 1 ) ] ];
     $pos += $size - 2;
     my $cksum = $bytes->[ $pos++ ];
-    printf( "Block %d: pos = %d, size = %d, flag = %d, cksum = %d\n",
+    printf( "Block #%d: pos = %d, size = %d, flag = %d (%s), cksum = %d\n",
         $num_block++,
-        $savepos, $size, $flag, $cksum
+        $savepos, $size, $flag, ( $flag ? 'data' : 'header' ), $cksum
     );
     output_data_block_info( $flag, $data );
 }
