6f131ef394fd91fd130afc9abce1a75757c79b14
Jorge Gonzalez <jorge.gonzalez@daikon.es>
2021-10-14 19:06:21 +0200

    tools: optimize lowmemsync to scale well ( O(n) instead of O(n^2) ) with number of symbols checked

--- tools/lowmemsym.pl
+++ tools/lowmemsym.pl
@@ -15,18 +15,36 @@ my @symbols = @ARGV;
 open my $map, "<", $map_file or
     die "** Could not open $map_file for reading\n";
 
+# first parse the map file and get a symbol table
+my %symbol_address;
 while ( my $line = <$map> ) {
     chomp $line;
-    foreach my $sym ( @symbols ) {
-        if ( $line =~ /^_?\Q$sym\E\s+=\s+\$([A-Fa-f\d]+)\s+; addr/ ) {
-            my $addr = hex( "0x$1" );
-            if ( $addr >= 0xC000 ) {
-                warn sprintf( "** Warning: symbol '%s' linked at address \$%X\n", $sym, $addr );
-            } else {
-                if ( defined( $opt_v ) ) {
-                    printf( "Symbol '%s' linked at address \$%X [OK]\n", $sym, $addr );
-                }
-            }
+    if ( $line =~ /^(_?[\w]+)\s+=\s+\$([A-Fa-f\d]+)\s+; addr/ ) {
+        $symbol_address{ $1 } = hex( $2 );
+    }
+}
+
+# then match each symbol against the symbol table
+print "Checking LOWMEM symbols...";
+
+my $errors;
+foreach my $sym ( @symbols ) {
+    my $addr = 0;
+    if ( defined( $symbol_address{ $sym } ) ) {
+        $addr = $symbol_address{ $sym };
+    }
+    if ( defined( $symbol_address{ '_' . $sym } ) ) {
+        $addr = $symbol_address{ '_' . $sym };
+    }
+    if ( $addr >= 0xC000 ) {
+        $errors++;
+        printf( "\n** Warning: symbol '%s' linked at address \$%X", $sym, $addr );
+    } else {
+        if ( defined( $opt_v ) ) {
+            printf( "Symbol '%s' linked at address \$%X [OK]\n", $sym, $addr );
         }
     }
 }
+
+print ( $errors ? "\n" : "OK\n" );
+exit 1 if $errors;
