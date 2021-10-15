ff73cfc52ae145695fcde243d48232723d0cd316
Jorge Gonzalez <jorge.gonzalez@daikon.es>
2021-10-15 10:48:53 +0200

    Makefile: modify mem target for better memory usage reporting

--- tools/r1size.sh
+++ tools/r1size.sh
@@ -10,14 +10,13 @@
 ## 
 ################################################################################
 
-echo "RAGE1 memory usage:"
-
 TOTAL_CODE=0
 TOTAL_DATA=0
 TOTAL_BSS=0
 
-printf "%-20s  %5s  %5s  %5s\n" "Object file" "CODE" "DATA" "BSS"
-echo "-------------------------------------------"
+echo "-------------------------------------------------"
+printf "%-20s  %5s  %5s  %5s\n" "OBJECT FILE" "CODE" "DATA" "BSS"
+echo "-------------------------------------------------"
 for ofile in engine/src/*.o engine/lowmem/*.o build/generated/*.o build/game_src/*.o build/generated/lowmem/*.o; do
 	CODE_SIZE=$( z88dk-z80nm "$ofile" | grep -P "Section code_.* \d+ bytes" | awk '{print $3}' | perl -ne 'BEGIN{$s=0;} $s+=$_; END{print $s;}' )
 	DATA_SIZE=$( z88dk-z80nm "$ofile" | grep -P "Section data_.* \d+ bytes" | awk '{print $3}' | perl -ne 'BEGIN{$s=0;} $s+=$_; END{print $s;}' )
@@ -29,5 +28,5 @@ for ofile in engine/src/*.o engine/lowmem/*.o build/generated/*.o build/game_src
 
 	printf "%-20s  %5d  %5d  %5d\n" "$( basename "$ofile" )" "$CODE_SIZE" "$DATA_SIZE" "$BSS_SIZE"
 done
-echo "-------------------------------------------"
+echo "-------------------------------------------------"
 printf "%-20s  %5d  %5d  %5d\n" "TOTAL" $TOTAL_CODE $TOTAL_DATA $TOTAL_BSS
