20257c7b19f64a50bdc2f9b66c74c55c7a1b8bd9
ZXjogv <zx@jogv.es>
2022-09-15 20:59:51 +0200

    music: enhanced converter script for arkos sources

--- tests/arkos/convert-arkos-source.pl
+++ tests/arkos/convert-arkos-source.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use utf8;
 
+use File::Copy;
+
 my @c_symbols = qw(
 	PLY_AKG_INIT
 	PLY_AKG_STOP
@@ -12,37 +14,58 @@ my @c_symbols = qw(
 	PLY_AKG_PLAYSOUNDEFFECT
 );
 
-print "section code_compiler\n\n";
+my @output;
+
+my $input = $ARGV[0];
+defined( $input ) or
+	die "usage: $0 <pasmo_asm_file>\n";
+
+open SRC, $input or
+	die "Could not open $input for reading...\n";
+
+push @output, "section code_compiler\n\n";
 
 my @all_symbols;
-while ( my $line = <> ) {
+while ( my $line = <SRC> ) {
 	chomp $line;
 	$line =~ s/\r//g;
 	$line =~ s/db /defb /g;
 	$line =~ s/dw /defw /g;
 	$line =~ s/\$\+/+/g;
 	if ( $line =~ /^(\w+)$/ ) {
-		print "\n$1:\n";
+		push @output, "\n$1:\n";
 		push @all_symbols, $1;
 		next;
 	}
 	$line =~ s/^\s+/\t/g;
 	if ( $line =~ /^(.+)\s+equ\s+(.*)$/ ) {
-		print "defc $1 = $2\n";
+		push @output, "defc $1 = $2\n";
 		next;
 	}
 	if ( $line =~ /^(\w+)\s+(.*)$/ ) {
-		print "$1:\n\t$2\n";
+		push @output, "$1:\n\t$2\n";
 		push @all_symbols, $1;
 		next;
 	}
-	print "$line\n";
+	push @output, "$line\n";
 }
 
 #print join( "\n", map { "public $_" } sort @all_symbols ), "\n";
 
 # print C symbols
-print "\n;;\n;; exported C symbols\n;;\n\n";
-print join( "\n", map { 
+push @output, "\n;;\n;; exported C symbols\n;;\n\n";
+push @output, join( "\n", map { 
 	sprintf( "defc _%s = %s\npublic _%s\n", lc( $_ ), $_, lc( $_ ) )
 	} sort @c_symbols ), "\n";
+close SRC;
+
+# output to temporary file
+my $output = "/tmp/perltmp.$$";
+open DST, ">$output" or
+	die "Could not open $output for writing...\n";
+print DST @output;
+close DST;
+
+# move temporary to original
+move $output, $input or
+	die "Could not rename $output to $input\n";
