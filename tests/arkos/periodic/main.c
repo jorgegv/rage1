03407bfcb18795e9c5e26855bfc85fc0d9287aee
ZXjogv <zx@jogv.es>
2022-09-21 19:31:27 +0200

    tests: fix arkos external declaration

--- tests/arkos/periodic/main.c
+++ tests/arkos/periodic/main.c
@@ -6,7 +6,7 @@ void ply_akg_init( void *song, unsigned int subsong ) __z88dk_callee;
 void ply_akg_play( void );
 void ply_akg_stop( void );
 void ply_akg_initsoundeffects( void *effects_table ) __z88dk_fastcall;
-void _ply_akg_playsoundeffect( unsigned int effect ) __z88dk_fastcall;
+void ply_akg_playsoundeffect( unsigned int effect ) __z88dk_fastcall;
 
 extern uint8_t song[];
 
