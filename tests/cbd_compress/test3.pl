4045548d9125127d63e7e38a0528eac0fee6367b
ZXjogv <zx@jogv.es>
2022-10-10 12:56:48 +0200

    tests: nitial tests for btile data dedupe

---
+++ tests/cbd_compress/test3.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+################################################################################
+##
+## RAGE1 - Retro Adventure Game Engine, release 1
+## (c) Copyright 2020 Jorge Gonzalez Villalonga <jorgegv@daikon.es>
+## 
+## This code is published under a GNU GPL license version 3 or later.  See
+## LICENSE file in the distribution for details.
+## 
+################################################################################
+
+use strict;
+use warnings;
+use utf8;
+
+
+
+1;
