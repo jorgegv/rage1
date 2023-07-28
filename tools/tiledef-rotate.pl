0c243fa69f223ae1c3c472d5b2f35e383c61e7e7
ZXjogv <zx@jogv.es>
2023-07-28 11:07:10 +0200

    tools: add tool for mirroring tile definitions in tiledef files

--- tools/tiledef-rotate.pl
+++ tools/tiledef-rotate.pl
@@ -6,11 +6,11 @@ use utf8;
 
 use Getopt::Std;
 
-our ( $opt_l, $opt_r, $opt_w, $opt_h );
-getopts('lrw:h:');
+our ( $opt_l, $opt_r, $opt_W, $opt_H );
+getopts('lrW:H:');
 
-( ( defined( $opt_l ) or defined( $opt_r ) ) and defined( $opt_w ) and defined( $opt_h ) ) or
-    die "usage: $0 <-l|-r> -w <width> -h <height>\n  -l: rotate tiledef counterclockwise\n  -r: rotate tiledef clockwise\n";
+( ( defined( $opt_l ) or defined( $opt_r ) ) and defined( $opt_W ) and defined( $opt_H ) ) or
+    die "usage: $0 <-l|-r> -W <width> -H <height>\n  -l: rotate tiledef counterclockwise\n  -r: rotate tiledef clockwise\n";
 
 while (my $line = <>) {
     if ( $line =~ m/^([\w_]+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/ ) {
@@ -18,11 +18,11 @@ while (my $line = <>) {
         if ( $opt_l ) {
             # for rotating left (counterclockwise), top-right corner will
             # become top-left, and we exchange width and height
-            printf "%-30s%d %d %d %d\n", $id, $opt_w - $c - $w, $r, $h, $w;
+            printf "%-30s%d %d %d %d\n", $id, $opt_W - $c - $w, $r, $h, $w;
         } else {
             # for rotating right (clockwise), bottom-left corner will become
             # top-left, and we exchange width and height
-            printf "%-30s%d %d %d %d\n", $id, $c, $opt_h - $r - $h, $h, $w;
+            printf "%-30s%d %d %d %d\n", $id, $c, $opt_H - $r - $h, $h, $w;
         }
     } else {
         print $line;
