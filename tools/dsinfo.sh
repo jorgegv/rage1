06a8657542478ec60cf6bf7dcf05bd4af9100df0
ZXjogv <zx@jogv.es>
2023-10-29 11:20:05 +0100

    tools: add element counters to dsinfo.sh report

--- tools/dsinfo.sh
+++ tools/dsinfo.sh
@@ -4,12 +4,15 @@ DATASETS=$( cd build/generated/datasets; ls -1 dataset*.bin* | cut -f2 -d_ | cut
 
 for i in $DATASETS; do
 	dir="build/generated/datasets/dataset_$i.src"
-	ARENA_SIZE=$( grep -E "^uint8_t all_dataset_btile_data" "$dir/main.c" | awk '{print $3}' )
-	TILEPTR_SIZE=$( grep -E '^uint8_t \*btile.*tiles' "$dir/main.c" | awk '{print $3}' | perl -ne '$a+=3*$_; END { print $a,"\n"; }' )
-	BTILEPOS_SIZE=$( grep -E '^struct btile_pos_s screen_' "$dir/main.c" | awk '{print $4}' | perl -ne '$a+=5*$_; END { print $a,"\n"; }' )
-	SCREEN_SIZE=$( grep -E "^struct map_screen_s all_screens" "$dir/main.c" | awk '{print $4}' | perl -ne '$a+=29*$_; END { print $a,"\n"; }' )
 	DS_SIZE=$( ls -l "build/generated/datasets/dataset_$i.bin.save" | awk '{print $5}' )
+	ARENA_SIZE=$( grep -E "^uint8_t all_dataset_btile_data" "$dir/main.c" | awk '{print $3}' )
+	TILEPTR_COUNT=$( grep -E '^uint8_t \*btile.*tiles' "$dir/main.c" | awk '{print $3}' | perl -ne '$a+=$_; END { print $a,"\n"; }' )
+	TILEPTR_SIZE=$(( TILEPTR_COUNT * 3 ))
+	BTILEPOS_COUNT=$( grep -E '^struct btile_pos_s screen_' "$dir/main.c" | awk '{print $4}' | perl -ne '$a+=$_; END { print $a,"\n"; }' )
+	BTILEPOS_SIZE=$(( BTILEPOS_COUNT * 5 ))
+	SCREEN_COUNT=$( grep -E "^struct map_screen_s all_screens" "$dir/main.c" | awk '{print $4}' | perl -ne '$a+=$_; END { print $a,"\n"; }' )
+	SCREEN_SIZE=$(( SCREEN_COUNT * 29 ))
 	REST=$(( DS_SIZE - ARENA_SIZE - TILEPTR_SIZE - BTILEPOS_SIZE - SCREEN_SIZE ))
-	printf "Dataset %2d: %5d bytes - Arena: %4d - BTdefs: %4d - BTpos: %4d - Screens: %4d - Rest: %4d\n" \
-		"$i" "$DS_SIZE" "$ARENA_SIZE" "$TILEPTR_SIZE" "$BTILEPOS_SIZE" "$SCREEN_SIZE" "$REST"
+	printf "Dataset %2d: %5db - Arena: %4db - BTdefs: %3d(%4db) - BTpos: %3d(%4db) - Screens: %3d(%4db) - Rest: %4db\n" \
+		"$i" "$DS_SIZE" "$ARENA_SIZE" "$TILEPTR_COUNT" "$TILEPTR_SIZE" "$BTILEPOS_COUNT" "$BTILEPOS_SIZE" "$SCREEN_COUNT" "$SCREEN_SIZE" "$REST"
 done
