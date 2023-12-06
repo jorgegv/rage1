b1be1c097a7a304a41d901684ae5e29eb0b0d9f8
ZXjogv <zx@jogv.es>
2023-12-06 16:31:24 +0100

    interrupts: fix race condition when checking interrupt_nesting_level

--- engine/include/rage1/interrupts.h
+++ engine/include/rage1/interrupts.h
@@ -34,8 +34,12 @@ extern uint8_t interrupt_nesting_level;
 void init_interrupts(void);
 void interrupt_enable_periodic_isr_tasks( void );
 
-// enable and disable ints only if needed
-#define intrinsic_di_if_needed()	do { if ( !interrupt_nesting_level++ ) intrinsic_di(); } while(0);
+// macros to enable and disable ints only if needed
+
+// if we are already in DI, DI again does not hurt. Just increment the nesting count with interrupts disabled
+#define intrinsic_di_if_needed()	do { intrinsic_di(); interrupt_nesting_level++; } while(0);
+
+// we should be in DI, so we can check nesting level safely. If 0 after decrementing, do an EI
 #define intrinsic_ei_if_needed()	do { if ( !--interrupt_nesting_level ) intrinsic_ei(); } while(0);
 
 #endif // _INTERRUPTS_H
