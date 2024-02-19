b295f6697a198e6f8d1bcc1a0233629d63a60c39
ZXjogv <zx@jogv.es>
2024-02-19 14:27:10 +0100

    local var optimization: bullet, optimized 150 bytes (WIP)

--- engine/banked_code/common/bullet.c
+++ engine/banked_code/common/bullet.c
@@ -92,14 +92,9 @@ void bullet_add( void ) {
     struct bullet_state_data_s *bs;
     uint8_t i;
     uint8_t h_dy, v_dx;
-    struct bullet_info_s *bi;
-    struct hero_info_s *hero;
 
-    bi = &game_state.bullet;
-    if ( bi->active_bullets == BULLET_MAX_BULLETS )
+    if ( game_state.bullet.active_bullets == BULLET_MAX_BULLETS )
         return;
-
-    hero = &game_state.hero;
 
     // search for an inactive slot in hero table
     i = BULLET_MAX_BULLETS;
@@ -108,40 +103,40 @@ void bullet_add( void ) {
         if ( ! GET_BULLET_FLAG( *bs, F_BULLET_ACTIVE ) ) {
             SET_BULLET_FLAG( *bs, F_BULLET_ACTIVE );
             game_state.bullet.active_bullets++;
-            h_dy = ( HERO_SPRITE_HEIGHT - bi->height ) / 2;
-            v_dx = ( HERO_SPRITE_WIDTH - bi->width ) / 2;
+            h_dy = ( HERO_SPRITE_HEIGHT - game_state.bullet.height ) / 2;
+            v_dx = ( HERO_SPRITE_WIDTH - game_state.bullet.width ) / 2;
 
-            if ( hero->movement.last_direction & MOVE_UP ) {
-                    bs->position.x.part.integer = hero->position.x.part.integer + v_dx;
-                    bs->position.y.part.integer = hero->position.y.part.integer - bi->height;
+            if ( game_state.hero.movement.last_direction & MOVE_UP ) {
+                    bs->position.x.part.integer = game_state.hero.position.x.part.integer + v_dx;
+                    bs->position.y.part.integer = game_state.hero.position.y.part.integer - game_state.bullet.height;
                     bs->dx = 0;
-                    bs->dy = -bi->movement.dy;
-                    bs->frame = bi->frames[ BULLET_SPRITE_FRAME_UP ];
+                    bs->dy = -game_state.bullet.movement.dy;
+                    bs->frame = game_state.bullet.frames[ BULLET_SPRITE_FRAME_UP ];
             }
-            if ( hero->movement.last_direction & MOVE_DOWN ) {
-                    bs->position.x.part.integer = hero->position.x.part.integer + v_dx;
-                    bs->position.y.part.integer = hero->position.ymax + 1;
+            if ( game_state.hero.movement.last_direction & MOVE_DOWN ) {
+                    bs->position.x.part.integer = game_state.hero.position.x.part.integer + v_dx;
+                    bs->position.y.part.integer = game_state.hero.position.ymax + 1;
                     bs->dx = 0;
-                    bs->dy = bi->movement.dy;
-                    bs->frame = bi->frames[ BULLET_SPRITE_FRAME_DOWN ];
+                    bs->dy = game_state.bullet.movement.dy;
+                    bs->frame = game_state.bullet.frames[ BULLET_SPRITE_FRAME_DOWN ];
             }
-            if ( hero->movement.last_direction & MOVE_LEFT ) {
-                    bs->position.x.part.integer = hero->position.x.part.integer - bi->width;
-                    bs->position.y.part.integer = hero->position.y.part.integer + h_dy;
-                    bs->dx = -bi->movement.dx;
+            if ( game_state.hero.movement.last_direction & MOVE_LEFT ) {
+                    bs->position.x.part.integer = game_state.hero.position.x.part.integer - game_state.bullet.width;
+                    bs->position.y.part.integer = game_state.hero.position.y.part.integer + h_dy;
+                    bs->dx = -game_state.bullet.movement.dx;
                     bs->dy = 0;
-                    bs->frame = bi->frames[ BULLET_SPRITE_FRAME_LEFT ];
+                    bs->frame = game_state.bullet.frames[ BULLET_SPRITE_FRAME_LEFT ];
             }
-            if ( hero->movement.last_direction & MOVE_RIGHT ) {
-                    bs->position.x.part.integer = hero->position.xmax + 1;
-                    bs->position.y.part.integer = hero->position.y.part.integer + h_dy;
-                    bs->dx = bi->movement.dx;
+            if ( game_state.hero.movement.last_direction & MOVE_RIGHT ) {
+                    bs->position.x.part.integer = game_state.hero.position.xmax + 1;
+                    bs->position.y.part.integer = game_state.hero.position.y.part.integer + h_dy;
+                    bs->dx = game_state.bullet.movement.dx;
                     bs->dy = 0;
-                    bs->frame = bi->frames[ BULLET_SPRITE_FRAME_RIGHT ];
+                    bs->frame = game_state.bullet.frames[ BULLET_SPRITE_FRAME_RIGHT ];
             }
-            bs->position.xmax = bs->position.x.part.integer + bi->width - 1;
-            bs->position.ymax = bs->position.y.part.integer + bi->height - 1;
-            bs->delay_counter = bi->movement.delay;
+            bs->position.xmax = bs->position.x.part.integer + game_state.bullet.width - 1;
+            bs->position.ymax = bs->position.y.part.integer + game_state.bullet.height - 1;
+            bs->delay_counter = game_state.bullet.movement.delay;
 
             // slot found, set game event and return
             SET_GAME_EVENT( E_BULLET_WAS_SHOT );
---
+++ tests/local_var_unoptimization/files.md
@@ -0,0 +1,30 @@
+# Optimization of local vars
+
+Files to review and associated baseline after optimization:
+
+- [x] engine/banked_code/common/animation.c - NO CHANGES
+- [x] engine/banked_code/common/beeper.c - NO CHANGES
+- [x] engine/banked_code/common/btile.c - NO CHANGES
+- [x] engine/banked_code/common/bullet.c - OK
+- [ ] engine/banked_code/common/enemy.c
+- [ ] engine/banked_code/common/hero.c
+- [x] engine/banked_code/128/btile.c - NO CHANGES
+- [x] engine/banked_code/128/dataset.c - NO CHANGES
+- [x] engine/banked_code/128/init.c - NO CHANGES
+- [x] engine/banked_code/128/tracker_arkos2.c - NO CHANGES
+- [x] engine/banked_code/128/tracker.c - NO CHANGES
+- [x] engine/banked_code/128/tracker_vortex2.c - NO CHANGES
+- [x] engine/banked_code/128/util.c - NO CHANGES
+
+Conclusions:
+
+- It's not a matter of "never use intermediate local vars" or "always use
+  intermediate local vars".  There are places where the code is complex and
+  the manual optimization pays off, and places where the code is simple and
+  you must let the compiler optimize.
+
+- Strategy should be to test every optimization and only pick one when the
+  code size is smaller. Case by case and function by function.
+
+- Always test with the full build baseline, do not compile each file
+  independently.
