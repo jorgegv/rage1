bab9fdbef7c6383b442b657189973115c0812c20
ZXjogv <zx@jogv.es>
2024-03-16 20:02:22 +0100

    tools: migrate BASIC bank loader to full assembler one for 128K mode
    Bank 7 was being corrupted by the interrupt routine in +2A/+3 models (disk
    motor control) at address 0xD200.  When using the old BASIC+ASM helper for
    bank switching an d loading, even with disabled interupts, BASIC kept
    enabling them when returning from the bswitch routine, and very sporadically
    an interrupt would trigger just after switching, and the byte at 0xD200 get
    corrupted.
    
    The solution has been to do all the bank switching and loading in asm by
    calling ROM LD_BYTES routine with the proper list of blocks, and with
    interrupts disabled.  The BASIC part now is just a 2-line program to load
    the asm loader and RAND USR to it, which continues the load.

--- tools/loadertool.pl
+++ tools/loadertool.pl
@@ -22,6 +22,9 @@ use lib "$FindBin::Bin/../lib";
 require RAGE::Config;
 
 my $basic_loader_name = 'loader.bas';
+my $asm_loader_name = 'asmloader.asm';
+
+my $main_bin_filename = 'main_CODE.bin';
 
 my $cfg = rage1_get_config();
 
@@ -46,6 +49,12 @@ sub gather_bank_binaries {
     return \%binaries;
 }
 
+sub get_main_bin_size {
+    my @stat_results = stat( $main_bin_filename );
+    return $stat_results[7];
+}
+
+# currently unused, I leave it here just in case
 sub generate_basic_loader {
     my ( $layout, $outdir, $loading_screen ) = @_;
     my $bas_loader = $outdir . '/' . $basic_loader_name;
@@ -91,6 +100,88 @@ sub generate_basic_loader {
     }
 }
 
+sub generate_assembler_loader {
+    my ( $layout, $outdir ) = @_;
+    my $asm_loader = $outdir . '/' . $asm_loader_name;
+
+    my @lines;
+
+    push @lines, <<EOF_HEADER
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This file has been generated automatically, do not edit!
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+EOF_HEADER
+;
+
+    push @lines, "\torg 0x8000";
+    push @lines, "\tdefc LD_BYTES = 1366\t;; ROM routine";
+    push @lines, "\textern bswitch";
+    push @lines, "";
+    push @lines, "\t;; do all loads with interrupts disabled so that bank 7 is not";
+    push @lines, "\t;; corrupted by +3DOS at address 0xD200";
+    push @lines, "\tdi";
+    push @lines, "";
+
+    # switch to each bank with the bank switching routine and load each bank content at 0xC000
+    foreach my $bank ( sort keys %$layout ) {
+        push @lines, "\tld a,$bank\t\t;; switch to bank $bank";
+        push @lines, "\tcall bswitch";
+        push @lines, "\tld a,0xff\t;; load data operation";
+        push @lines, sprintf( "\tld de,%d\t;; number of bytes to load", $layout->{ $bank }{'size'} );
+        push @lines, "\tld ix,0xc000\t;; destination address";
+        push @lines, "\tscf";
+        push @lines, "\tcall LD_BYTES\t;; load block";
+        push @lines, "";
+    }
+
+    # switch back to bank 0
+    push @lines, "\t;; switch to bank 0 and load main binary";
+    push @lines, "\txor a";
+    push @lines, "\tcall bswitch";
+    push @lines, "";
+
+    # load main program code at base code address and start execution
+    my $main_code_start = ( $cfg->{'interrupts_128'}{'base_code_address'} =~ /^0x/ ?
+        hex( $cfg->{'interrupts_128'}{'base_code_address'} ) :
+        $cfg->{'interrupts_128'}{'base_code_address'}
+    );
+    push @lines, "\tld a,0xff";
+    push @lines, sprintf( "\tld de,%d", get_main_bin_size );
+    push @lines, "\tld ix,$main_code_start";
+    push @lines, "\tscf";
+    push @lines, "\tcall LD_BYTES";
+    push @lines, "";
+
+    push @lines, "\t;; Start execution";
+    push @lines, "\tdi";
+    push @lines, "\tjp $main_code_start";
+    push @lines, "\t;; shouldn't return";
+
+    push @lines, <<EOF_BSWITCH
+
+bswitch:
+        ;; register A contains the bank to switch to
+        and     0x07            ; get 3 low bits only
+        ld      b,a             ; save for later
+        ld      a,(0x5b5c)      ; get last value from SYS.BANKM
+        and     0xf8            ; save 5 top bits
+        or      b               ; mix new value with old
+        ld      bc,0x7ffd       ; set the port number
+        ld      (0x5b5c),a      ; ...store the new value to SYS.BANKM
+        out     (c),a           ; ...and select the new bank
+        ret                     ; back to BASIC
+EOF_BSWITCH
+;
+
+    # that's it, output the BASIC program
+    open my $asm, ">", $asm_loader
+        or die "\n** Error: could not open $asm_loader for writing\n";
+    foreach my $line ( @lines ) {
+        printf $asm "%s\n", $line;
+    }
+}
+
 ##
 ## Main
 ##
@@ -110,4 +201,5 @@ my ( $input_dir, $output_dir ) = ( $opt_i, $opt_o );
 
 my $bins = gather_bank_binaries( $input_dir );
 
-generate_basic_loader( $bins, $output_dir, $loading_screen );
+#generate_basic_loader( $bins, $output_dir, $loading_screen );
+generate_assembler_loader( $bins, $output_dir );
